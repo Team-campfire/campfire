317aa009f6f642f448fc332a4a4fbac0a2aac63a
anya-tralshawala <55996101+anya-tralshawala@users.noreply.github.com>
2021-02-20 15:08:57 -0500

    Serving HTML with express
    -issues serving css
    
    Co-Authored-By: Rachel  <61204083+rachel-wg@users.noreply.github.com>

--- server.js
+++ server.js
@@ -1,3 +1,31 @@
+//Express stuff
+const express = require('express')
+const app = express()
+const port = 3000
+const path = require('path')
+app.use(express.static(path.resolve('/public/css')))
+app.get('/', (req, res) => { //basic get request, should serve index.html
+    res.sendFile(__dirname+'/index.html')
+}) 
+
+app.listen(port, () => { 
+  console.log('listening :))')
+})
+
+
+/*
+function css (request, response) {
+  if (request.url.indexOf(".css") !== -1){
+    var file = fs.readFileSync(`${request.url}`, {'encoding' : 'utf8'});
+    response.writeHead(200, {'Content-Type' : 'text/css'});
+    response.write(file);
+    response.end();
+  }
+}
+*/
+
+
+/*
 const http = require('http')
 const fs = require('fs')
 
@@ -6,4 +34,6 @@ const server = http.createServer((req, res) => {
   fs.createReadStream('index.html').pipe(res)
 })
 
+
 server.listen(process.env.PORT || 3000)
+*/
