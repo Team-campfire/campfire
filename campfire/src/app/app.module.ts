cc1d0164bac4d1f5bb872d3f849be313d2eb1d79
anya-tralshawala <55996101+anya-tralshawala@users.noreply.github.com>
2021-03-17 13:44:53 -0400

    Added components
    -fully implemented landing page into angular
    -fully implemented join event page into angular
    - started create event page implementation
    -restructured assets and user flow
    - restructured components and the contents
    
    Co-Authored-By: Rachel  <61204083+rachel-wg@users.noreply.github.com>

--- campfire/src/app/app.module.ts
+++ campfire/src/app/app.module.ts
@@ -1,20 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AjaxComponent } from './ajax/ajax.component';
+import { JoinEventComponent } from './join-event/join-event.component';
+import { CreateEventComponent } from './create-event/create-event.component';
+
+
+const appRoutes: Routes = [
+  {path: '', pathMatch: 'full', component: HomeComponent},
+  {path: 'createEvent', component: CreateEventComponent},
+  {path: "joinEvent", component: JoinEventComponent},
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AjaxComponent
+    AjaxComponent,
+    JoinEventComponent,
+    CreateEventComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
