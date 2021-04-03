ea27b1501ce19f113011f3c07244317148052e22
anya-tralshawala <55996101+anya-tralshawala@users.noreply.github.com>
2021-04-03 11:55:20 -0400

    added event-feed, and event-start components, created json file structure and added to shared db
    - added event-feed component
    - added event-start child component to create event component
    - created json file structure
    - created and uploaded json to team campfire shared db
    
    Co-Authored-By: Rachel  <61204083+rachel-wg@users.noreply.github.com>

--- campfire/src/app/app.module.ts
+++ campfire/src/app/app.module.ts
@@ -8,7 +8,7 @@ import { HomeComponent } from './home/home.component';
 import { AjaxComponent } from './ajax/ajax.component';
 import { JoinEventComponent } from './join-event/join-event.component';
 import { CreateEventComponent } from './create-event/create-event.component';
-import { EventBasicsComponent } from './create-event/event-basics/event-basics.component';
+import { EventStartComponent } from './create-event/event-start/event-start.component';
 import { EventActivitiesComponent } from './create-event/event-activities/event-activities.component';
 import { EventTransportationComponent } from './create-event/event-transportation/event-transportation.component';
 import { FinishComponent } from './create-event/finish/finish.component';
@@ -16,19 +16,23 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { OwnerDashboardComponent } from './dashboard/owner-dashboard/owner-dashboard.component';
 import { EditorDashboardComponent } from './dashboard/editor-dashboard/editor-dashboard.component';
 import { ViewerDashboardComponent } from './dashboard/viewer-dashboard/viewer-dashboard.component';
+import { EventFeedComponent } from './event-feed/event-feed.component';
+import { EventBasicsComponent } from './create-event/event-basics/event-basics.component';
 
 
 const appRoutes: Routes = [
   {path: '', pathMatch: 'full', component: HomeComponent},
   {path: 'createEvent', component: CreateEventComponent},
   {path: "joinEvent", component: JoinEventComponent},
+  {path: "eventStart", component: EventStartComponent},
   {path: "eventBasics", component: EventBasicsComponent},
   {path: "eventActivities", component: EventActivitiesComponent},
   {path: "eventTransportation", component: EventTransportationComponent},
   {path: "finish", component: FinishComponent},
   {path: "ownerDashboard", component: OwnerDashboardComponent},
   {path: "editorDashboard", component: EditorDashboardComponent}, 
-  {path: "viewerDashboard", component: ViewerDashboardComponent} 
+  {path: "viewerDashboard", component: ViewerDashboardComponent},
+  {path: "eventFeed", component: EventFeedComponent} 
 
 ];
 
@@ -39,14 +43,16 @@ const appRoutes: Routes = [
     AjaxComponent,
     JoinEventComponent,
     CreateEventComponent,
-    EventBasicsComponent,
+    EventStartComponent,
     EventActivitiesComponent,
     EventTransportationComponent,
     FinishComponent,
     DashboardComponent,
     OwnerDashboardComponent,
     EditorDashboardComponent,
-    ViewerDashboardComponent
+    ViewerDashboardComponent,
+    EventFeedComponent,
+    EventBasicsComponent
   ],
   imports: [
     BrowserModule,
