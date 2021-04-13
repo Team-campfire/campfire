03b1ec149ad70855b01a954c161df1234ea3fd73
anya-tralshawala <55996101+anya-tralshawala@users.noreply.github.com>
2021-04-12 23:19:46 -0400

    update R viz and frontend using action and method
    - updated R visualizations
    - updated data-vaz.component.html using action/method
    - need to figure out how to get visualization to show up

--- campfire/src/app/data-viz/data-viz.component.ts
+++ campfire/src/app/data-viz/data-viz.component.ts
@@ -7,31 +7,31 @@ import { HttpService } from '../http.service';
   styleUrls: ['./data-viz.component.css']
 })
 export class DataVizComponent implements OnInit {
-  // constructor() { }
-  // ngOnInit(): void {
-  // }
-    constructor(private httpService: HttpService) {
-      this.eventCategory = "";
-    }
-
-    ngOnInit(): void {
-    }
-    public eventCategory: string;
-
-    public getEventCategory(): void {
-      this.httpService.sendGetRequest('/event').subscribe((data) => {
-
-        /*
-         * This quickly gets tedious.
-         * Use external libraries.
-         */
-        interface RandomObj {
-          rnum: string;
-        }
-
-        let obj: RandomObj = JSON.parse(JSON.stringify(data));
-        this.eventCategory = obj.rnum;
-      })
-    }
+  constructor() { }
+  ngOnInit(): void {
   }
-// }
+}
+  // constructor(private httpService: HttpService) {
+  //     this.eventCategory = "";
+  //   }
+
+  //   ngOnInit(): void {
+  //   }
+  //   public eventCategory: string;
+
+  //   public getEventCategory(): void {
+  //     this.httpService.sendGetRequest('/event').subscribe((data) => {
+
+  //       /*
+  //        * This quickly gets tedious.
+  //        * Use external libraries.
+  //        */
+  //       interface RandomObj {
+  //         rnum: string;
+  //       }
+
+  //       let obj: RandomObj = JSON.parse(JSON.stringify(data));
+  //       this.eventCategory = obj.rnum;
+  //     })
+  //   }
+  // }
