4e8ef570f669845a2e8c9f5bd18ff6ee4f26d124
mkokkatt <47791277+mkokkatt@users.noreply.github.com>
2021-04-22 17:16:35 -0400

    form revamp

--- campfire/src/app/create-event/event-start/event-start.component.ts
+++ campfire/src/app/create-event/event-start/event-start.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-event-start',
@@ -7,9 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventStartComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  	this.email = "";
+  	this.eventname = "";
+  	this.name = "";
+  	this.friend = "";
+
+   }
 
   ngOnInit(): void {
   }
 
+
+   public email: string;
+  public eventName: string;
+  public name: string;
+  public friend: string;
+
+
+
+   //get request 
+   onClickSubmit(mi : any): void {
+
+       this.http.get<any>('/getTeam/' + mi.conf + '/' + mi.city + '/' + mi.win).subscribe((data) => {
+
+      /*
+       * This quickly gets tedious.
+       * Use external libraries.
+       */
+
+
+      interface RandomObj {
+        team: string;
+      }
+
+      let obj: RandomObj = JSON.parse(JSON.stringify(data));
+      this.team = obj.team;
+
+    })
+   }
 }
