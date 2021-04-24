da7afd147564847532d6be091a8b92da5e343d7c
anya-tralshawala <55996101+anya-tralshawala@users.noreply.github.com>
2021-04-24 10:08:44 -0400

    updated owner dashboard page and made view dashboard readonly
    - view dashboard will not be able to be edited
    - fixed and added fields in owner dashboard

--- campfire/src/app/dashboard/viewer-dashboard/viewer-dashboard.component.ts
+++ campfire/src/app/dashboard/viewer-dashboard/viewer-dashboard.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../../http.service';
 
 @Component({
-  selector: 'app-viewer-dashboard',
-  templateUrl: './viewer-dashboard.component.html',
-  styleUrls: ['./viewer-dashboard.component.css']
+  selector: 'app-owner-dashboard',
+  templateUrl: './owner-dashboard.component.html',
+  styleUrls: ['./owner-dashboard.component.css']
 })
-export class ViewerDashboardComponent implements OnInit {
+export class OwnerDashboardComponent implements OnInit {
 
-  constructor() { }
+  // public desc: string;
+  // public addy: string;
+  // public oaddy: string;
+  // public desc: string;
+  // public eventName: string;
+  // public date: string;
+  desc = '';
+  addy = '';
+  oaddy = '';
+  eventName = '';
+  date = '';
+  eventDesc = '';
+  clubOrgDesc = '';
 
+  constructor(private httpService: HttpService) {
+    this.eventName = "Web Sci Demo";
+    this.addy = "N/A";
+    this.oaddy = "https://rensselaer.webex.com/meet/tralsa";
+    this.date = "4/27/2021";
+    this.desc = "This is a really cool event and you should join!"
+    this.eventDesc = "This is our final presentation"
+    this.clubOrgDesc = "This is an amazing club"
+
+    // let btn = document.getElementById("coolbutton");
+    // if (btn) {
+    //   btn.addEventListener("click", (e:Event) => this.fillForm());
+    // }
+  }
   ngOnInit(): void {
   }
 
+  fillForm() {
+    // button click handler
+    //  this.eventName = "Web Sci Demo";
+    //  this.addy = "N/A";
+    //  this.oaddy = "https://rensselaer.webex.com/meet/tralsa";
+    //  this.date = "4/27/2021";
+    //  this.desc = "This is a really cool event and you should join!"
+  }
 }
