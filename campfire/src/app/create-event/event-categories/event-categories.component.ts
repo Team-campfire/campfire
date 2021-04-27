a11bbad5908d29465e58178508a0e08eff12c28f
teddy-clark <edwardeclarkiii@gmail.com>
2021-04-27 00:49:26 -0400

    update event categories radio

--- campfire/src/app/create-event/event-categories/event-categories.component.ts
+++ campfire/src/app/create-event/event-categories/event-categories.component.ts
@@ -7,14 +7,14 @@ import { HttpService } from '../../http.service';
   templateUrl: './event-categories.component.html',
   styleUrls: ['./event-categories.component.css']
 })
+
 export class EventCategoriesComponent implements OnInit {
 
-  constructor(private http: HttpClient) { 
-  this.eventCategory = "";
+  constructor(private http: HttpClient) {
+    this.eventCategory = "";
   	this.clubCategory = "";
   	this.eventDesc = "";
   	this.clubDesc = "";
-
   }
 
   ngOnInit(): void {
@@ -25,48 +25,26 @@ export class EventCategoriesComponent implements OnInit {
   public eventDesc: string;
   public clubDesc: string;
 
+  //post request for posting category details
+  onClickSubmit(mi : any): void {
+    this.http.post<any>('/submitCategories', {
+      eventCategory:  this.eventCategory,
+      eventDescription:  mi.eventDescription,
+      clubCategory: this.clubCategory,
+      clubDescription:  mi.clubDescription
+    })
+    .subscribe((data)  => { console.log(data); }, (err) => {
+      console.log("Error", err);
+    });
+  }
 
-
-    //post request for posting category details
-   onClickSubmit(mi : any): void {
-
-
-       this.http.post<any>('/submitCategories',
-{
-eventCategory:  this.eventCategory,
-eventDescription:  mi.eventDescription,
-clubCategory: this.clubCategory,
-clubDescription:  mi.clubDescription
-})
-.subscribe(
-(data)  => {
-console.log(data);
-},
-(err) => {
-
-console.log("Error", err);
-
-}
-
-);
-    }
-
-
-    setCategory(e: string): void   
-  {  
+  setCategory(e: string): void {
   //set event category
+    this.eventCategory = e;
+  }
 
-        this.eventCategory = e;  
-          
-  }  
-  
-
-    setClub(e: string): void   
-  {  
+  setClub(e: string): void {
 //set club category
-        this.clubCategory = e;  
-          
-  }  
-
-
+    this.clubCategory = e;
+  }
 }
