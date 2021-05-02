c9ec19d1b80ae4ff6e5843976c32f8996c5ae364
teddy-clark <edwardeclarkiii@gmail.com>
2021-05-01 21:10:05 -0400

    add form modules

--- campfire/src/app/create-event/event-categories/event-categories.component.ts
+++ campfire/src/app/create-event/event-categories/event-categories.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient} from '@angular/common/http';
 import { HttpService } from '../../http.service';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';  
 
 @Component({
   selector: 'app-event-categories',
