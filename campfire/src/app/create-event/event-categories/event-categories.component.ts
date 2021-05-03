0af73876ab7c68cd6e04c2f49e125c7bb1c2ee64
teddy-clark <edwardeclarkiii@gmail.com>
2021-05-03 00:55:13 -0400

    remove excess and add sources

--- campfire/src/app/create-event/event-categories/event-categories.component.ts
+++ campfire/src/app/create-event/event-categories/event-categories.component.ts
@@ -11,6 +11,12 @@ import { CommonModule } from '@angular/common';
 })
 export class EventCategoriesComponent implements OnInit {
 
+  constructor(private http: HttpClient, private fb: FormBuilder) {
+  }
+
+  ngOnInit(): void {
+  }
+
   catgeoryForm = this.fb.group({
     eventDescription: [''],
     clubDescription: [''],
@@ -19,133 +25,59 @@ export class EventCategoriesComponent implements OnInit {
     }),
     clubCategories: this.fb.group({
       clubCategory: ['']
-    }),
+    })
     // aliases
     // aliases: this.fb.array([
     //   this.fb.control('')
     // ])
   });
 
-  profileForm = this.fb.group({
-  firstName: ['', Validators.required],
-  lastName: [''],
-  address: this.fb.group({
-    street: [''],
-    city: [''],
-    state: [''],
-    zip: ['']
-  }),
-  colors: this.fb.group({
-    color: ['']
-  }),
-  // aliases
-  aliases: this.fb.array([
-    this.fb.control('')
-  ])
-});
-
-get aliases() {
-  return this.profileForm.get('aliases') as FormArray;
-}
-
-addAlias() {
-  this.aliases.push(this.fb.control(''));
-}
-
-  constructor(private http: HttpClient, private fb: FormBuilder) {
-    // this.eventCategory = "";
-    // this.clubCategory = "";
-    // this.eventDesc = "";
-    // this.clubDesc = "";
-  }
-
-  ngOnInit(): void {
-  }
-
-  // public eventCategory: string;
-  // public clubCategory: string;
-  // public eventDesc: string;
-  // public clubDesc: string;
-
-
-
-  //post request for posting category details
-  // onClickSubmit(mi: any): void {
-  //
-  //
-  //   this.http.post<any>('/submitCategories',
-  //     {
-  //       eventCategory: this.eventCategory,
-  //       eventDescription: mi.eventDescription,
-  //       clubCategory: this.clubCategory,
-  //       clubDescription: mi.clubDescription
-  //     })
-  //     .subscribe(
-  //       (data) => {
-  //         console.log(data);
-  //       },
-  //       (err) => {
-  //
-  //         console.log("Error", err);
-  //
-  //       }
-  //
-  //     );
-  // }
-
   onClickSubmit() {
-    // TODO: Use EventEmitter with form value
-    // alert(this.profileForm.value.colors.color);
-    // alert(this.profileForm.get('firstName')!.value);
-    // alert(this.profileForm.firstName.value);
-
-    // alert(formData.getAll('name'));
-
     this.http.post<any>('/submitCategories', {
-      // myfirstname:  this.profileForm.value.firstName,
-      // colorchoice:  this.profileForm.value.colors.color,
-
       eventCategory: this.catgeoryForm.value.eventCategories.eventCategory,
       eventDescription: this.catgeoryForm.value.eventDescription,
       clubCategory: this.catgeoryForm.value.clubCategories.clubCategory,
       clubDescription: this.catgeoryForm.value.clubDescription
-
-      // eventCategory: this.eventCategory,
-      // eventDescription: mi.eventDescription,
-      // clubCategory: this.clubCategory,
-      // clubDescription: mi.clubDescription
     })
     .subscribe((data)  => { console.log(data); }, (err) => {
       console.log("Error", err);
     });
   }
 
-  onSubmit1() {
-    // TODO: Use EventEmitter with form value
-    alert(this.profileForm.value.colors.color);
-    // alert(this.profileForm.get('firstName')!.value);
-    // alert(this.profileForm.firstName.value);
-    // console.warn(this.profileForm.value);
+  // get aliases() {
+  //   return this.profileForm.get('aliases') as FormArray;
+  // }
+  //
+  // addAlias() {
+  //   this.aliases.push(this.fb.control(''));
+  // }
 
-    // let formData: any = new FormData();
-    // this.fname = this.profileForm.value.firstName;
-    // formData.append("name", this.profileForm.get('firstName')!.value);
-    // alert(formData.getAll('name'));
+  // sources for reactive forms:
+  // more documentation and work in typescript_to_node repository
 
-    // this.http.post<any>('/submitCategories', {
-    //
-    //   myfirstname:  this.profileForm.value.firstName,
-    //   colorchoice:  this.profileForm.value.colors.color
-    //
-    //   // fname: this.fname
-    //   // eventCategory:  this.eventCategory,
-    //   // eventDescription:  mi.eventDescription,
-    //   // clubCategory: this.clubCategory,
-    //   // clubDescription:  mi.clubDescription
-    // })
-    // .subscribe((data)  => { console.log(data); }, (err) => {
-    //   console.log("Error", err);
-    // });
-  }
+  // https://angular.io/guide/reactive-forms
+  // https://v2.angular.io/docs/ts/latest/guide/reactive-forms.html
+  // https://www.elite-corner.com/2018/11/angular-json-pipe-example.html
+  // https://stackoverflow.com/questions/43713558/how-to-get-a-single-value-from-formgroup
+
+  // https://stackoverflow.com/questions/44602420/angular2-4-populate-form-after-request
+  // https://angular.io/api/forms/FormGroup
+  // https://stackoverflow.com/questions/49078286/angular-5-reactive-forms-radio-button-group/49078441
+  // https://stackoverflow.com/questions/40647073/angular-2-accessing-data-from-formarray
+  // https://www.w3schools.com/jsref/met_console_warn.asp
+  // https://www.pluralsight.com/guides/how-to-submit-form-data-using-angular
+  // https://stackoverflow.com/questions/43800907/typescript-strictnullchecks-object-is-possibly-null
+  // https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-0.html#non-null-assertion-operator
+
+  // https://developer.mozilla.org/en-US/docs/Web/API/FormData/getAll
+  // https://www.tutorialspoint.com/expressjs/expressjs_form_data.htm
+  // https://developer.mozilla.org/en-US/docs/Web/API/FormData/Using_FormData_Objects
+  // https://stackoverflow.com/questions/16104078/appending-array-to-formdata-and-send-via-ajax
+  // https://angular.io/tutorial/toh-pt6
+  // https://stackoverflow.com/questions/34475523/how-to-pass-url-arguments-query-string-to-a-http-request-on-angular
+  // https://angular.io/guide/http#setup-for-server-communication
+
+  // https://stackoverflow.com/questions/47895748/json-data-is-not-showing-up
+  // https://stackoverflow.com/questions/44779067/json-data-not-showing-in-html/44779141
 
 }
