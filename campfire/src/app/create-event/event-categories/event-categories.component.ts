eae31b4a8a811f5d22ab13d2208fdc4c04e54828
teddy-clark <edwardeclarkiii@gmail.com>
2021-05-03 01:08:00 -0400

    more sources

--- campfire/src/app/create-event/event-categories/event-categories.component.ts
+++ campfire/src/app/create-event/event-categories/event-categories.component.ts
@@ -52,32 +52,44 @@ export class EventCategoriesComponent implements OnInit {
   //   this.aliases.push(this.fb.control(''));
   // }
 
-  // sources for reactive forms:
-  // more documentation and work in typescript_to_node repository
-
-  // https://angular.io/guide/reactive-forms
-  // https://v2.angular.io/docs/ts/latest/guide/reactive-forms.html
-  // https://www.elite-corner.com/2018/11/angular-json-pipe-example.html
-  // https://stackoverflow.com/questions/43713558/how-to-get-a-single-value-from-formgroup
-
-  // https://stackoverflow.com/questions/44602420/angular2-4-populate-form-after-request
-  // https://angular.io/api/forms/FormGroup
-  // https://stackoverflow.com/questions/49078286/angular-5-reactive-forms-radio-button-group/49078441
-  // https://stackoverflow.com/questions/40647073/angular-2-accessing-data-from-formarray
-  // https://www.w3schools.com/jsref/met_console_warn.asp
-  // https://www.pluralsight.com/guides/how-to-submit-form-data-using-angular
-  // https://stackoverflow.com/questions/43800907/typescript-strictnullchecks-object-is-possibly-null
-  // https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-0.html#non-null-assertion-operator
-
-  // https://developer.mozilla.org/en-US/docs/Web/API/FormData/getAll
-  // https://www.tutorialspoint.com/expressjs/expressjs_form_data.htm
-  // https://developer.mozilla.org/en-US/docs/Web/API/FormData/Using_FormData_Objects
-  // https://stackoverflow.com/questions/16104078/appending-array-to-formdata-and-send-via-ajax
-  // https://angular.io/tutorial/toh-pt6
-  // https://stackoverflow.com/questions/34475523/how-to-pass-url-arguments-query-string-to-a-http-request-on-angular
-  // https://angular.io/guide/http#setup-for-server-communication
-
-  // https://stackoverflow.com/questions/47895748/json-data-is-not-showing-up
-  // https://stackoverflow.com/questions/44779067/json-data-not-showing-in-html/44779141
-
 }
+
+// sources for reactive forms:
+// more documentation and work in typescript_to_node repository
+
+// https://stackoverflow.com/questions/40367713/get-reference-to-currently-active-htmlelement
+// https://stackoverflow.com/questions/43376081/angular2-retrieve-all-elements-with-class-name
+
+// https://angular.io/guide/reactive-forms
+// https://v2.angular.io/docs/ts/latest/guide/reactive-forms.html
+// https://www.elite-corner.com/2018/11/angular-json-pipe-example.html
+// https://stackoverflow.com/questions/43713558/how-to-get-a-single-value-from-formgroup
+
+// https://stackoverflow.com/questions/44602420/angular2-4-populate-form-after-request
+// https://angular.io/api/forms/FormGroup
+// https://stackoverflow.com/questions/49078286/angular-5-reactive-forms-radio-button-group/49078441
+// https://stackoverflow.com/questions/40647073/angular-2-accessing-data-from-formarray
+// https://www.itsolutionstuff.com/post/angular-10-radio-button-reactive-form-exampleexample.html
+// // https://stackoverflow.com/questions/39956579/angular2-reactive-forms-formcontrol-for-radio-buttons
+// https://www.w3schools.com/jsref/met_console_warn.asp
+// https://www.pluralsight.com/guides/how-to-submit-form-data-using-angular
+// https://stackoverflow.com/questions/43800907/typescript-strictnullchecks-object-is-possibly-null
+// https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-0.html#non-null-assertion-operator
+
+// https://developer.mozilla.org/en-US/docs/Web/API/FormData/getAll
+// https://www.tutorialspoint.com/expressjs/expressjs_form_data.htm
+// https://developer.mozilla.org/en-US/docs/Web/API/FormData/Using_FormData_Objects
+// https://stackoverflow.com/questions/16104078/appending-array-to-formdata-and-send-via-ajax
+// https://angular.io/tutorial/toh-pt6
+// https://stackoverflow.com/questions/34475523/how-to-pass-url-arguments-query-string-to-a-http-request-on-angular
+// https://angular.io/guide/http#setup-for-server-communication
+
+// https://stackoverflow.com/questions/47895748/json-data-is-not-showing-up
+// https://stackoverflow.com/questions/44779067/json-data-not-showing-in-html/44779141
+// https://stackoverflow.com/questions/39152071/cant-bind-to-formgroup-since-it-isnt-a-known-property-of-form
+// https://stackoverflow.com/questions/25471856/express-throws-error-as-body-parser-deprecated-undefined-extended
+// https://stackoverflow.com/questions/26347394/node-js-with-express-bodyparser-unable-to-obtain-form-data-from-post-reques
+// https://stackoverflow.com/questions/49642926/formgroup-get-vs-formgroup-controls-in-reactive-form-angular
+// https://www.positronx.io/how-to-use-angular-8-httpclient-to-post-formdata/
+// https://stackoverflow.com/questions/51422528/how-to-submit-form-with-method-post-in-angular-2-using-formgroup
+// https://angular.io/tutorial/toh-pt2#style-the-selected-hero
