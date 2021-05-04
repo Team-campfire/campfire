a859ed20be6a8e2d5457a2ba0a3561559c3ac401
mkokkatt <47791277+mkokkatt@users.noreply.github.com>
2021-05-03 22:47:46 -0400

    event code functionality

--- campfire/src/app/join-event/join-event.component.ts
+++ campfire/src/app/join-event/join-event.component.ts
@@ -1,15 +1,46 @@
+
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpService } from '../http.service';
+import { Router } from '@angular/router'; 
 
 @Component({
   selector: 'app-join-event',
   templateUrl: './join-event.component.html',
   styleUrls: ['./join-event.component.css']
 })
+
 export class JoinEventComponent implements OnInit {
 
-  constructor() { }
+
+
+  constructor(private http: HttpClient, private router: Router) {
+    this.code = "";
+    
+  }
 
   ngOnInit(): void {
   }
 
+  public code: string;
+
+
+  //post request for transportation details
+  onClickSubmit(mi: any): void {
+      this.router.navigate(['/viewerDashboard']);
+    this.http.post<any>('/saveEventCode', {
+    
+      eventCode: mi.code
+    })
+      .subscribe((data) => { console.log(data); }, (err) => {
+        console.log("Error", err);
+      });
+  }
+
+
 }
+
+
+
+
+
